d4484f59d7aa6e96835b85daa1870fe6bb39a58b
류호성 <52982845+Hoseong-Ryu@users.noreply.github.com>
2021-05-06 09:57:41 +0900

    Apply .gitignore

--- TrestApi/routes/users.js
+++ TrestApi/routes/users.js
@@ -23,7 +23,30 @@ router.post('/sign-up', function (req, res, next) {
       res.status(400).json({error: 'BadRequest'});
     }
     else{
-      res.status(200).send()
+      try {
+        const id = req.body.userid;
+    
+        // jwt.sign() 메소드: 토큰 발급
+        const token = jwt.sign({
+          id,
+        }, process.env.JWT_SECRET, {
+          expiresIn: '1m', // 1분
+        });
+    
+        return res.json({
+          code: 200,
+          message: '토큰이 발급되었습니다.',
+          token,
+        });
+      }
+    
+      catch (error) {
+        console.error(error);
+        return res.status(500).json({
+          code: 500,
+          message: '서버 에러',
+        });
+      }
     }
     
   });
